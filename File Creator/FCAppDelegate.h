ef4813fb462b928099830382915d65e3f5547eff
Jaime Marquínez Ferrándiz <jaimemf93@gmail.com>
2013-02-07 21:31:17 +0100

    Update documentation

--- File Creator/FCAppDelegate.h
+++ File Creator/FCAppDelegate.h
@@ -16,11 +16,15 @@
 /// The objects that takes care of the services calls and the creation of files
 @property FCFileCreator *fileCreator;
 
-/// Show the preferences
-/// @bug When the window has already been closed once, the call fails.
+/** Show the preferences
+ * @bug When the window has already been closed once, the call fails.
+ * @param sender The sender of the message.
+ */
 -(IBAction)showPreferences:(id)sender;
 -(IBAction)quitApp:(id)sender;
-/** Open the homepage */
+/** Open the homepage 
+ * @param sender The sender of the message.
+ */
 -(IBAction)openHomepage:(id)sender;
 
 @end
--- File Creator/FCFileCreator.h
+++ File Creator/FCFileCreator.h
@@ -1,29 +1,48 @@
 #import <Foundation/Foundation.h>
 
+/**
+ FCFileCreator listen for services messages and creates the files at the selected paths.
+ */
 @interface FCFileCreator : NSObject
 
 /** @name Creating files */
 /**
- * The low level call for creating files
+ * The lower level call for creating files
+ * @param filename The file name (with an optional extension).
+ * @param path The directory where the file will be created.
+ * @param contents The file contents.
+ * @param attributes The file attributes.
  */
 -(void)createFile:(NSString *)filename atPath:(NSString *)path contents:(NSData *)contents attributes:(NSDictionary *)attributes;
 /**
  * Create a file with the desired extension
+ * @param filename The file name (without extension).
+ * @param extension the file extension, without point, e.g: "txt".
+ * @param path The directory where the file will be created.
+ * @param contents The file contents.
+ * @param attributes The file attributes.
  */
 -(void)createFile:(NSString *)filename withExtension:(NSString *)extension atPath:(NSString *)path contents:(NSData *)contents attributes:(NSDictionary *)attributes;
 
 /**
- * @param pboard The pasteboard with the selection
+ * @param pboard The pasteboard with the selection.
  * @returns The directory of the selected files
  */
 -(NSString *)directoryFromPasteboard:(NSPasteboard *)pboard;
 /**
  *Create a file at the path of the selected files
+ * @param filename The file name (without extension).
+ * @param pboard The pasteboard with the selection.
+ * @param extension the file extension, without point, e.g: "txt".
+ * @param contents The file contents.
+ * @param attributes The file attributes.
  */
 -(void)createFile:(NSString *)filename withExtension:(NSString *)extension pasteboard:(NSPasteboard *)pboard contents:(NSData *)contents attributes:(NSDictionary *)attributes;
 /**
  * Same as createFile:withExtension:pasteboard:contents:attributes: but with predefined values
- * @param pboard The pasteboard with the selection
+ * @param filename The file name (without extension).
+ * @param extension the file extension, without point, e.g: "txt".
+ * @param pboard The pasteboard with the selection.
  * @sa createFile:withExtension:pasteboard:contents:attributes:
  */
 -(void)createFile:(NSString *)filename withExtension:(NSString *)extension pasteboard:(NSPasteboard *)pboard;
@@ -31,7 +50,10 @@
  * Create a script file
  * Depending on the user defaults it will give execution permissions
  * and add a shebang.
- * @param pboard The pasteboard with the selection
+ * @param filename The file name (without extension).
+ * @param extension the file extension, without point, e.g: "txt".
+ * @param pboard The pasteboard with the selection.
+ * @param shebang The shebang needed for running the scriot, should start with `#!`. For example: `#!/usr/bin/python`.
  * @see createFile:withExtension:pasteboard:contents:attributes:
  * @warning The shebang format is not defined, for now it should start by "#!", but it could change in the future
  */
@@ -39,15 +61,21 @@
 
 /** @name Services */
 /** Create a file with "txt" extension
- * @param pboard The pasteboard with the selection
+ * @param pboard The pasteboard with the selection.
+ * @param data Some data
+ * @param error Error
  */
 -(void)createFile:(NSPasteboard *)pboard userData:(NSString *)data error:(NSString **)error;
 /** Create a script with "py" extension
- * @param pboard The pasteboard with the selection
+ * @param pboard The pasteboard with the selection.
+ * @param data Some data
+ * @param error Error
  */
 -(void)createPythonScript:(NSPasteboard *)pboard userData:(NSString *)data error:(NSString **)error;
 /** Create a script with "sh" extension
- * @param pboard The pasteboard with the selection
+ * @param pboard The pasteboard with the selection.
+ * @param data Some data
+ * @param error Error
  */
 -(void)createShellScript:(NSPasteboard *)pboard userData:(NSString *)data error:(NSString **)error;
 
